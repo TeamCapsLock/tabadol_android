9be296793c0e8c86d42c130b25829c814ef4a04e
MHD22 <mhd.sad22@gmail.com>
2021-06-16 02:57:42 +0300

    save changes

--- app/src/main/java/com/example/tabadol/api/MyRetrofit.java
+++ app/src/main/java/com/example/tabadol/api/MyRetrofit.java
@@ -1,7 +1,6 @@
 package com.example.tabadol.api;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
